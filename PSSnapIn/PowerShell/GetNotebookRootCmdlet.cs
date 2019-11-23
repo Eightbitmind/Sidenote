8828b0e8b1f717aacffedae02390ddc018897514
aeulitz <aeulitz@outlook.com>
2019-11-22 18:19:38 -0800

    Add GetNotebookRoot cmdlet, Test project

---
+++ PSSnapIn/PowerShell/GetNotebookRootCmdlet.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Office.Interop.OneNote;
+using Sidenote.DOM;
+using Sidenote.Serialization;
+using System.Management.Automation;
+
+namespace Sidenote.Client
+{
+	[Cmdlet("Get", "NotebookRoot")]
+	public class GetNotebookRootCmdlet : Cmdlet
+	{
+		protected override void ProcessRecord()
+		{
+			var app = new Application();
+			IFormatter<IRoot> notebooksFormatter = FormatterManager.NotebooksFormatter;
+			IRoot root = notebooksFormatter.Deserialize(app, null);
+			WriteObject(root);
+		}
+	}
+}
---
+++ Tests/EnumerateBooksSectionsPages.ps1
@@ -0,0 +1,14 @@
+﻿foreach ($notebook in (Get-NotebookRoot).Notebooks)
+{
+	Write-Host "notebook `"$($notebook.Name)`""
+
+	foreach ($section in $notebook.Children)
+	{
+		Write-Host "`tsection `"$($section.Name)`""
+
+		foreach ($page in $section.Children)
+		{
+			Write-Host "`t`tpage `"$($page.Name)`""
+		}
+	}
+}
