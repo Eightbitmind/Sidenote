2243ad44a7be8d9db3292e309543bfb92432ef40
Andreas Eulitz <aeulitz@outlook.com>
2020-04-21 22:17:27 -0700

    Get-XmlDoc cmdlet

---
+++ BinaryPart/PowerShell/GetXmlDoc.cs
@@ -0,0 +1,23 @@
+﻿using System.Management.Automation;
+using System.Xml;
+
+namespace Sidenote.PowerShell
+{
+	[Cmdlet("Get", "XmlDoc")]
+	public class GetXmlDoc : Cmdlet
+	{
+		[Parameter(
+			HelpMessage = "InnerXml",
+			ValueFromPipeline = true,
+			Mandatory = true,
+			Position = 0)]
+		public string InnerXml;
+
+		protected override void ProcessRecord()
+		{
+			var doc = new XmlDocument();
+			doc.LoadXml("<InnerXmlRoot>" + this.InnerXml + "</InnerXmlRoot>");
+			WriteObject(doc);
+		}
+	}
+}
